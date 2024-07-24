68413701a2ea24f8f6727f8d3d6820bfb00c0ccb
Emmanuel K. Mensah <ekmpizarro@gmail.com>
2024-07-24 01:39:12 +0000

    modified functions in views.py

--- mySite/polls/views.py
+++ mySite/polls/views.py
@@ -1,18 +1,21 @@
-from django.shortcuts import render, get_list_or_404
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.http import Http404, HttpResponse
 from . models import Question
 
 # Create your views here.
 def index(request):
-    
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
+
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {
+        'question ': question
+    }
+    return render(request, "polls/detail.html", context)
+
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
